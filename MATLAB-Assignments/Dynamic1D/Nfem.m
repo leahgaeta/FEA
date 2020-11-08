07104ef54ef83c8467b7bf1fbf0cf4bae12a90f7
Leah Gaeta <32988741+leahgaeta@users.noreply.github.com>
2020-11-08 11:25:35 -0800

    Delete Nfem.m

--- MATLAB-Assignments/Dynamic1D/Nfem.m
+++
@@ -1,6 +0,0 @@
-function N = Nfem(x)
-% 1D FEM shape function in parent (psi) domain
-
-global nn ne xn conn h;
-
-N = [.5*(1-x) .5*(1+x)];    % FEM linear shape functions in 1D isoparametric parent domain
